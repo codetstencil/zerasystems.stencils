a8723ca7d6bd9aa7a51e2f9b09387c4b3fad6b79
Ayo Dahunsi <ayo@dolasoft.com>
2020-05-13 00:35:33 -0600

    Added/Updated Mega Menu Nanite

---
+++ default/src/Default/MegaMenu.Functions.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using ZeraSystems.CodeNanite.Expansion;
+using ZeraSystems.CodeStencil.Contracts;
+
+namespace ZeraSystems.CodeNanite.Default
+{
+    public partial class MegaMenu
+    {
+        private int _tablesCount;
+
+        private void MainFunction()
+        {
+            _tablesCount = GetTables().Count;
+            var height = (_tablesCount / 3) ;
+            height = height * 80;
+
+            //var value1 = GetExpansionString("MEGA_MENU_HEIGHT");
+            ExpanderUpdater(height.ToString(), "MEGA_MENU_HEIGHT",height);
+            //var value2 = GetExpansionString("MEGA_MENU_HEIGHT");
+
+            var settings = GetExpansionString("GRID_SETTINGS");
+
+            if (!settings.IsBlank())
+            {
+                //var items = settings.Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+                //_gridConfiguration = items.Select(item => item.Split('='))
+                //    .ToDictionary(keyValue => keyValue[0], keyValue => keyValue[1], StringComparer.InvariantCultureIgnoreCase);
+            }
+
+            var dropDown = "<a" +
+                         Extensions.SetText("href", "#") +
+                         Extensions.SetText("class", "dropdown-toggle nav-link text-dark") +
+                         Extensions.SetText("data-toggle", "dropdown") +
+                         Extensions.SetText("role", "button") +
+                         Extensions.SetText("aria-haspopup","true") +
+                         Extensions.SetText("aria-expanded","false") +
+                         ">Tables</a>";
+
+            var text = dropDown.AddCarriage() + MenuLinks();
+            AppendText(text.Li("class", "dropdown dropdown-cols-2 nav-item"));
+            
+        }
+
+        private string MenuLinks()
+        {
+            BuildSnippet(null);
+            var lists = SplitList<ISchemaItem>(GetTables(), 3);
+
+            foreach (var list in lists)
+            {
+                var text = string.Empty;
+                foreach (var item in list)
+                {
+                    text += MenuLink(item.TableName, item.TableName.Pluralize()+"/Index")
+                        .Li("class", "nav-link text-dark").AddCarriage();
+                }
+
+                BuildSnippet(text.Ul().HtmlTag("div"),0);
+            }
+            return BuildSnippet().HtmlTag("div", "class", "dropdown-menu");
+        }
+
+        private string MenuLink(string table, string page)
+        {
+            var result = "<a"+
+                Extensions.SetText("class", "nav-link text-dark") +
+                 Extensions.SetText("asp-area", "") +
+                 Extensions.SetText("asp-page", "/" + page) + ">" + table + "</a>";
+            return result.Indent(44);
+        }
+
+        public static IEnumerable<List<T>> SplitList<T>(List<T> locations, int nSize = 5)
+        {
+            for (var i = 0; i < locations.Count; i += nSize)
+            {
+                yield return locations.GetRange(i, Math.Min(nSize, locations.Count - i));
+            }
+        }
+
+        private static string SimpleIndent(string text, int indent, int tab)
+        {
+            var regex = new Regex("<.+?>");
+            var matches = regex.Matches(text);
+            var i = indent;
+            var st = new StringBuilder();
+            foreach (Match match in matches)
+            {
+                var groups = match.Groups;
+                var a = groups[0].Value;
+                st.Append(a.PadLeft(i));
+                i += tab;
+            }
+
+            return st.ToString();
+        }
+
+    }
+}
---
+++ default/src/Default/MegaMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using ZeraSystems.CodeNanite.Expansion;
+using ZeraSystems.CodeStencil.Contracts;
+
+namespace ZeraSystems.CodeNanite.Default
+{
+    /// <summary>
+    /// There are 10 elements in the String Array used by the
+    ///  0 - This is the name of the publisher
+    ///  1 - This is the title of the Code Nanite
+    ///  2 - This is the
+    ///  3 - Version Number
+    ///  4 - Label of the Code Nanite
+    ///  5 - Namespace
+    ///  6 - Release Date
+    ///  7 - Name to use for Expander Label
+    ///  8 - Indicates that the Nanite is Schema Dependent
+    ///  9 - RESERVED
+    /// 10 - Online Help URL
+    /// </summary>
+    [Export(typeof(ICodeStencilCodeNanite))]
+    [CodeStencilCodeNanite(new[]
+    {
+        "Zera Systems Inc.",
+        "Mega Menu Generator",
+        "Generate a Mega menu with table names as as the sub menu items.",
+        "1.0",
+        "MegaMenu",
+        "ZeraSystems.CodeNanite.Default",
+        "05/01/2020",
+        "CS_MEGA_MENU",
+        "1",
+        "",
+        "https://codestencil.com/zerasystems.schema/currenttable"
+    })]
+    public partial class MegaMenu : ExpansionBase, ICodeStencilCodeNanite
+    {
+        public string Input { get; set; }
+        public string Output { get; set; }
+        public int Counter { get; set; }
+        public List<string> OutputList { get; set; }
+        public List<ISchemaItem> SchemaItem { get; set; }
+        public List<IExpander> Expander { get; set; }
+        public List<string> InputList { get; set; }
+
+        public void ExecutePlugin()
+        {
+            ExpandedText.Clear();
+            Initializer(SchemaItem, Expander);
+            MainFunction();
+            Output = FormatHtml(ExpandedText.ToString(), 16);
+
+            //Output =   ExpandedText.ToString();
+        }
+    }
+}
